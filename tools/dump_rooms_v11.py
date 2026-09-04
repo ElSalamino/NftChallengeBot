464df92c0318e86c79dbbef7e618fa7f2bf3989b
ElSalamino <86099774+ElSalamino@users.noreply.github.com>
2026-09-04 10:15:24 +0200

    Prepara audit stanze dungeon v11

---
+++ tools/dump_rooms_v11.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import json
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(ROOT))
+sys.path.insert(0, str(ROOT / 'wiki'))
+
+import genera_wiki_v10 as wiki
+import bilanciamento
+
+
+data = wiki.build_data()
+rooms = []
+for room in data['dungeon']['rooms']:
+    name = room['name']
+    rooms.append({
+        'name': name,
+        'intro': room.get('intro'),
+        'weight': room.get('weight'),
+        'pct': room.get('pct'),
+        'actions': room.get('actions', []),
+        'config': bilanciamento.DUNGEON_CONFIG.get('stanze', {}).get(name, {}),
+    })
+print('ROOM_DUMP_BEGIN')
+print(json.dumps(rooms, ensure_ascii=False, indent=2, sort_keys=True))
+print('ROOM_DUMP_END')
